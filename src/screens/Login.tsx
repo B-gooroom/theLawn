9074192f11fc57ca0213ecdcd0b5d905d79746ea
sean <j.sean1316@gmail.com>
2023-02-26 21:52:45 +0900

    feat: [Login] 페이지 추가

---
+++ src/screens/Login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function LoginScreen({navigation}: any): JSX.Element {
+  console.log('nav', navigation);
+
+  return (
+    <div>
+      <div>Login</div>
+    </div>
+  );
+}
